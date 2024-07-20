c6a05910b15ba64a1286d1e0a90bfe4b921de6c3
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-07-20 12:25:39 +0530

    Functions and Arrays

---
+++ Week1/Day0_1.java
@@ -0,0 +1,18 @@
+public class Day0_1 {
+    static void printArr(int[] arr){
+        //for each loop
+        for(int i:arr)
+            System.out.print(i+" ");
+    }
+    // Return the sum of your arr: 1,2,3,4,5
+    // Print an array in reverse order: 5,4,3,2,1
+    // Reverse the array(optional)
+    public static void main(String[] args) {
+        int[] arr= {1,2,3,4,5};
+        printArr(arr);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
