01d4f268e8186f5e715a293490ba8ea818d5cfa6
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-07-21 09:37:49 +0530

    Q33 merged

--- Week0/Doubts.java
+++ Week0/Doubts.java
@@ -48,31 +48,6 @@ public class Doubts {
         //         System.out.print("*");
         //     System.out.println();
         // }
-// q33 pattern 
-int m=10;
-for (int i = 1; i <=m; i++) {
-    for(int k=1;k<=m-i;k++){
-        System.out.print("  ");
-    }
-    for (int j = 1; j <=i; j++) {
-        if(i==j) System.out.print(0+" ");
-        else System.out.print(m-i+j+" ");
-    }
-    for (int j = 2; j <=i; j++) {
-        System.out.print(m-j+1+" ");
-    }
-    System.out.println();
-}        
-//                   0 
-//                 9 0 9 
-//               8 9 0 9 8
-//             7 8 9 0 9 8 7
-//           6 7 8 9 0 9 8 7 6
-//         5 6 7 8 9 0 9 8 7 6 5
-//       4 5 6 7 8 9 0 9 8 7 6 5 4
-//     3 4 5 6 7 8 9 0 9 8 7 6 5 4 3
-//   2 3 4 5 6 7 8 9 0 9 8 7 6 5 4 3 2
-// 1 2 3 4 5 6 7 8 9 0 9 8 7 6 5 4 3 2 1
     }
 }
 
---
+++ Week1/Day1.java
@@ -0,0 +1,5 @@
+public class Day1 {
+    public static void main(String[] args) {
+        
+    }
+}
