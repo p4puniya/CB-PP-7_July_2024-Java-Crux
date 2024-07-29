8addca8e974e2c1ea9909fa4ffdfe28153e3f44b
Student ApnaCollege <bsreya0906@gmail.com>
2024-07-29 10:42:51 +0530

    Ques20

--- Week0/Solutions/Ques20.java
+++ Week0/Solutions/Ques20.java
@@ -13,48 +13,52 @@ public class Ques20{
           *    
           
           */
-                
           int n = 7; 
-                int a = (n+1)/2;
-                for(int i = a; i>= 1; i--){
-                    for(int j = 1; j<= i;  j++){
-                        if(j==i){
-                        System.out.print("* ");
-                    }
-                    else {
-                        System.out.print("  ");
-                    }
+          int a = (n+1)/2;
+        
+          // for upper half
+        for(int i = a; i>= 1; i--){
+            for(int j = 1; j<= i;  j++){
+                if(j==i){
+                   System.out.print("* ");
                 }
-                    for(int j = 1; j<= a-i; j++){
+               else {
                         System.out.print("  ");
-                    }
-                    for(int j =2; j<= a-i; j++){
-                        System.out.print("  ");
-                    }
-                    for(int j =1; j<= a-i && j<= 1 ; j++){
-                        System.out.print("* ");
-                    }
-                    System.out.println();
                 }
-                for(int i = 2; i<= a; i++){
-                    for(int j = 1; j<= i;  j++){
-                        if(j==i){
-                        System.out.print("* ");
-                    }
-                    else {
-                        System.out.print("  ");
-                    }
-                }
-                    for(int j = 1; j<= a-i; j++){
-                        System.out.print("  ");
-                    }
-                    for(int j =2; j<= a-i; j++){
-                        System.out.print("  ");
-                    }
-                    for(int j =1; j<= a-i && j<= 1 ; j++){
-                        System.out.print("* ");
-                    }
-                    System.out.println();
-                } 
             }
+            for(int j = 1; j<= a-i; j++){
+                        System.out.print("  ");
+            }
+            for(int j =2; j<= a-i; j++){
+                        System.out.print("  ");
+            }
+            for(int j =1; j<= a-i && j<= 1 ; j++){
+                System.out.print("* ");
+            }
+                    System.out.println();
         }
+
+        // for lower half 
+        for(int i = 2; i<= a; i++){     // i will start with 2 && it is the inverse of upper half
+            for(int j = 1; j<= i;  j++){
+                if(j==i){
+                   System.out.print("* ");
+                }
+                else {
+                        System.out.print("  ");
+                }
+            }
+            for(int j = 1; j<= a-i; j++){
+                System.out.print("  ");
+            }
+            for(int j =2; j<= a-i; j++){
+                System.out.print("  ");
+            }
+            for(int j =1; j<= a-i && j<= 1 ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+} 
+        
