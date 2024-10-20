faf74c36ca48ee244608c181121577d87ce955c5
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-10-20 10:42:34 +0530

    Lecture 22

---
+++ Lecture22/Stk.java
@@ -0,0 +1,24 @@
+package Lecture22;
+import java.util.Stack;
+public class Stk {
+    public static void main(String[] args) {
+        Stack<Integer> stk= new Stack<>();
+        // System.out.println(stk.empty());
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        stk.push(2);
+        stk.push(3);
+        // System.out.println(stk.peek());
+        // System.out.println(stk.pop());
+        System.out.println(stk.search(1));
+        
+        stk.setSize(10);
+    }
+}
